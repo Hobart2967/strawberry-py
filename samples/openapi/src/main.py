692f753f7d0dcacd9705686e6b787c31f233d9cf
Marco Klein <marco.klein@avsn.de>
2020-09-30 18:03:38 +0200

    add payload

--- samples/openapi/src/main.py
+++ samples/openapi/src/main.py
@@ -21,3 +21,58 @@ def handle_http_request(http_request):
   controller_handler = ControllerHandler.getinstance()
   http_response = controller_handler.handleRequest(http_request)
   return http_response
+
+"""
+multipart payload
+{
+	'headers': {
+		'Content-Type': 'multipart/form-data; boundary=--------------------------978974156610236089704874',
+		'apiKey': '20',
+		'User-Agent': 'PostmanRuntime/7.24.1',
+		'Accept': '*/*',
+		'Cache-Control': 'no-cache',
+		'Postman-Token': '8c42147d-9119-4a60-8f14-840dec078212',
+		'Host': 'localhost:3000',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Connection': 'keep-alive',
+		'Content-Length': 382
+	},
+	'path': '/v2/pet/123',
+	'pathParameters': {
+		'proxy': 'v2%2Fpet%2F123'
+	},
+	'requestContext': {
+		'accountId': 'offlineContext_accountId',
+		'resourceId': 'offlineContext_resourceId',
+		'apiId': 'offlineContext_apiId',
+		'stage': 'dev',
+		'requestId': 'offlineContext_requestId_',
+		'identity': {
+			'cognitoIdentityPoolId': 'offlineContext_cognitoIdentityPoolId',
+			'accountId': 'offlineContext_accountId',
+			'cognitoIdentityId': 'offlineContext_cognitoIdentityId',
+			'caller': 'offlineContext_caller',
+			'apiKey': 'offlineContext_apiKey',
+			'sourceIp': '127.0.0.1',
+			'cognitoAuthenticationType': 'offlineContext_cognitoAuthenticationType',
+			'cognitoAuthenticationProvider': 'offlineContext_cognitoAuthenticationProvider',
+			'userArn': 'offlineContext_userArn',
+			'userAgent': 'PostmanRuntime/7.24.1',
+			'user': 'offlineContext_user'
+		},
+		'authorizer': {
+			'principalId': 'offlineContext_authorizer_principalId'
+		},
+		'resourcePath': '/{proxy*}',
+		'httpMethod': 'POST'
+	},
+	'resource': '/{proxy*}',
+	'httpMethod': 'POST',
+	'queryStringParameters': {
+		'TestKey': ['12', '11']
+	},
+	'stageVariables': None,
+	'body': '----------------------------978974156610236089704874\r\nContent-Disposition: form-data; name="name"\r\n\r\nDampf\r\n----------------------------978974156610236089704874\r\nContent-Disposition: form-data; name="firstName"\r\n\r\nHans\r\n----------------------------978974156610236089704874\r\nContent-Disposition: form-data; name="test"\r\n\r\n123\r\n----------------------------978974156610236089704874--\r\n',
+	'isOffline': True
+}
+"""
